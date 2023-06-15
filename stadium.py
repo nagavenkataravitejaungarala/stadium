0d6c2c4d185b7cc2ce74bd95db26898a19a7957d
nagavenkataravitejaungarala <130119502+nagavenkataravitejaungarala@users.noreply.github.com>
2023-06-15 17:08:52 +0530

    Update stadium.py

--- stadium.py
+++ stadium.py
@@ -3,30 +3,38 @@ import streamlit as st
 class CricketStadium:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.booking_list = [0] * capacity
+        self.stadium_list = [{'seat_number': i+1, 'status': 'Available'} for i in range(capacity)]
+        self.order_data = []
 
     def book_seat(self, seat_number):
-        if self.booking_list[seat_number-1] == 0:
-            self.booking_list[seat_number-1] = 1
+        seat = self.stadium_list[seat_number-1]
+        if seat['status'] == 'Available':
+            seat['status'] = 'Booked'
+            self.order_data.append(seat)
             st.success(f"Seat {seat_number} booked successfully!")
         else:
             st.warning(f"Seat {seat_number} is already booked.")
 
     def cancel_seat(self, seat_number):
-        if self.booking_list[seat_number-1] == 1:
-            self.booking_list[seat_number-1] = 0
+        seat = self.stadium_list[seat_number-1]
+        if seat['status'] == 'Booked':
+            seat['status'] = 'Available'
+            self.order_data = [order for order in self.order_data if order['seat_number'] != seat_number]
             st.success(f"Seat {seat_number} cancelled successfully!")
         else:
             st.warning(f"Seat {seat_number} is not booked.")
 
     def view_availability(self):
         st.write("Current seat availability:")
-        for i, status in enumerate(self.booking_list):
-            seat_number = i + 1
-            if status == 0:
-                st.write(f"Seat {seat_number}: Available")
-            else:
-                st.write(f"Seat {seat_number}: Booked")
+        for seat in self.stadium_list:
+            st.write(f"Seat {seat['seat_number']}: {seat['status']}")
+
+        st.write("\nOrder data:")
+        if self.order_data:
+            for order in self.order_data:
+                st.write(f"Seat {order['seat_number']} is booked.")
+        else:
+            st.write("No seat has been booked yet.")
 
 # Creating an instance of the CricketStadium
 stadium = CricketStadium(capacity=50)
